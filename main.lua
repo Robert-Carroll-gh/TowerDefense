cc9d6c4c8eeaf895095873495736a1ec503272e6
Robert Carroll <robert.carroll.eportfolio@gmail.com>
2024-03-19 22:44:10 -0600

    first commit

---
+++ main.lua
@@ -0,0 +1,12 @@
+print("hello world")
+
+x = 45
+
+print(x)
+
+love.load = function() end
+
+love.draw = function()
+	love.graphics.print("hello world", 300, 150)
+	love.graphics.rectangle("fill", 10, 10, 50, 100)
+end
