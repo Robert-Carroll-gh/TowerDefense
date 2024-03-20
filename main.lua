187872692c1793b495162b591f33565caeeaf75c
Derago <deragoduong@gmail.com>
2024-03-20 02:22:35 -0600

    Update1

--- main.lua
+++ main.lua
@@ -1,13 +1,5 @@
-print("hello world")
-
-x = 45
-
-print(x)
-
-love.load = function() end
+require("map1")
 
 love.draw = function()
-	love.graphics.print("hello world", 300, 150)
-	love.graphics.rectangle("fill", 10, 10, 50, 100)
+    map.draw()
 end
---hi
