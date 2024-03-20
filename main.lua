d61ded0935a1636d0dc68bdf4e7cab6042eacdc4
Poyo <poyoblizzard@gmail.com>
2024-03-20 00:46:23 -0600

    base mob & some main.lua changes
    mob1.lua goal = trying to make it so enemy will disappear when hp = 0 (still struggling)

--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
+require("mob1")
 print("hello world")
 
 x = 45
@@ -9,4 +10,5 @@ love.load = function() end
 love.draw = function()
 	love.graphics.print("hello world", 300, 150)
 	love.graphics.rectangle("fill", 10, 10, 50, 100)
+	enemy.draw()
 end
---
+++ mob1.lua
@@ -0,0 +1,37 @@
+enemy = {
+    x = 200,
+	y = 150,
+	radius = 20,
+	hp = 3,
+
+		
+	
+	draw = function()	
+		love.graphics.setColor(1, 1, 1)
+		
+		love.graphics.print(enemy.hp, enemy.x, enemy.y + 50)
+		
+		love.graphics.circle("fill", enemy.x, enemy.y, enemy.radius)
+	end
+}
+
+enemy.update = function()
+	
+	local shouldDrawEnemy = true
+	local enemyIsAlive = enemy.hp >= 1
+	local shouldDrawEnemy = enemyIsAlive
+
+	if shouldDrawEnemy == true then
+		enemy.draw()
+	end
+	
+	if enemy.hp == 0 then
+		shouldDrawEnemy = false
+	end
+end
+
+love.keypressed = function(key)
+	if key == "j" then
+		enemy.hp = enemy.hp - 1
+	end
+end
