14d97dc8f0bf169e4c4f8e01fd206d1bf5187dcb
Robert Carroll <robert.carroll.eportfolio@gmail.com>
2024-03-20 20:03:10 -0600

    made cursor position always visible
    will make this and other dev util displays toggleable later

--- main.lua
+++ main.lua
@@ -2,6 +2,7 @@ Bullet = require("bullet")
 Projectiles = require("projectiles")
 require("tower")
 require("mob1")
+require("utils")
 
 love.load = function()
     tower = Tower:new(50,50)
@@ -12,6 +13,8 @@ love.draw = function()
     tower:draw()
 	enemy.draw()
     Projectiles:draw()
+
+    drawMoreInfo()
 end
 
 love.update = function(dt)
--- utils.lua
+++ utils.lua
@@ -1,3 +1,13 @@
+
+
+function drawMoreInfo()
+    love.graphics.setColor(1,1,1)
+    local mousePositionText = "   X:"..love.mouse.getX() .. " Y: " .. love.mouse.getY()
+    love.graphics.print(mousePositionText, love.mouse.getX(), love.mouse.getY())
+
+end
+
+
 function normalize(x, y, newLength)
     local length = math.sqrt(x^2 + y^2)
     x = x / length
