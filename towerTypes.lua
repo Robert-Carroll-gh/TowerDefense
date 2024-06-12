4d7f90fcf31c5cd605df6059073a3469ab93d59d
Robert Carroll <robert.carroll.eportfolio@gmail.com>
2024-06-12 17:26:19 -0600

    fae puff full upgrade path complete

--- towerTypes.lua
+++ towerTypes.lua
@@ -54,11 +54,13 @@ towerTypes.fae_puff = {
 towerTypes.fairy_cloud = {
     cost = 50,
     upgradesFrom = "fae_puff",
-    upgradesTo = "none",
-    color = { 1, 0, 0.8 },
+    upgradesTo = "sacred_haven",
+    color = { 1, 0, 1 },
     width = 55,
     height = 55,
+    fireRate = 5,
     manaGeneration = 50,
+
     generateMana = function(self)
         local newMana = World.mana + self.manaGeneration
         if newMana <= World.manaCap then
@@ -92,14 +94,47 @@ towerTypes.fairy_cloud = {
 }
 
 towerTypes.sacred_haven = {
-    cost = 100,
+    cost = 1000,
     upgradesFrom = "fairy_cloud",
     upgradesTo = "none",
-    -- color = { 1, 0, 0.5 },
+    color = { 1, 0, 1 },
     width = 60,
     height = 60,
     manaGeneration = 150,
     fireRate = 1.5,
+
+    generateMana = function(self)
+        local newMana = World.mana + self.manaGeneration
+        if newMana <= World.manaCap then
+            World.mana = newMana
+        else
+            World.mana = World.manaCap
+        end
+        World.graphicHandler:flashCircle(self.x, self.y, 75, Utils.setAlpha(ManaColor, 0.5))
+    end,
+
+    new = function(self, x, y)
+        World.manaCap = World.manaCap + 1000
+
+        local t = World.towerHandler.Tower.new(self, x, y)
+        t.shotTimer = World.timerHandler:new(self.fireRate, function()
+            t:generateMana()
+        end, true)
+
+        self.__index = self
+        setmetatable(t, self)
+
+        return t
+    end,
+
+    update = function(self, dt)
+        if self.hp <= 0 then
+            self.kill = true
+        end
+        if self.kill == true then
+            self.shotTimer.kill = true
+        end
+    end,
 
     --[[ new = function(self, x, y)
         towerTypes.fae_puff.new(self, x, y)
